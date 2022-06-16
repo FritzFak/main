35d2d196c9219bc9f851dbbbde58882f9ce7f092
Faker <fakss14@mail.ru>
2022-06-16 18:57:24 +0300

    new

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 65536; i++) {// переберем числа в написанном цикле!
+            char c = (char) i;// преобразуем их в символы и выведем в консоль!
+            System.out.println(i + " - " + c);
+        }
+    }
+}
